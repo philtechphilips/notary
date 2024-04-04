f4206a77dc5d1b333c8c77372c9d279caee5bd04
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2024-04-04 11:20:38 +0100

    feat: about page updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,47 +2,26 @@
 
 
 @section('content')
-    <section class="w-full h-screen bg-cover bg-center bg-no-repeat relative"
-        style="background-image: url('{{ asset('images/hero.jpeg') }}')">
+    <section class="w-full h-[700px] bg-cover bg-center bg-no-repeat relative"
+    style="background-image: url('{{ asset('images/notary-services.png') }}')">
 
         <div class="w-full md:px-20 px-5 py-10 absolute bottom-0 glassmorphism">
-            <h1 class="text-white font-[Inter] md:text-6xl text-4xl font-semibold  text-center md:text-left">Your trusted
-                partner for all your notary and legal needs.</h1>
-            <p class="text-white py-4 md:text-xl text-center md:text-left">Founded with a commitment to excellence and client
-                satisfaction, we have been serving individuals and businesses with integrity and professionalism for years.
+            <h1 class="text-white font-[Inter] md:text-6xl text-4xl font-semibold  text-center md:text-left">
+                Your trusted partner for all your notary and legal needs.
+            </h1>
+            <p class="text-white py-4 md:text-xl text-center md:text-left">
+                Founded with a commitment to excellence and client satisfaction, we have been serving individuals and businesses with integrity and professionalism for years.
             </p>
         </div>
+        <div class="bg-black bg-opacity-60 w-full h-[700px] top-0 left-0"></div>
     </section>
 
-    <section class="w-full md:px-20 px-5 md:py-24 py-16 flex flex-col md:flex-row justify-center items-start gap-16">
-        <div class="md:w-1/2">
-            <img src="{{ asset('images/about.jfif') }}" alt="" class="w-full rounded-t-lg">
-            <div class="mt-6">
-                <h1 class="text-[#121212] font-[Inter] text-4xl font-medium text-center md:text-left ">Our Story</h1>
-                <p class="mt-2 text-center md:text-left ">In today's fast-paced world, where time is of the essence and precision is paramount, the
-                    significance of reliable and efficient notary and legal services cannot be overstated. Whether you're
-                    navigating the complexities of a real estate transaction, solidifying the terms of a crucial loan
-                    agreement, or seeking astute legal counsel to safeguard your interests, our dedicated team stands ready
-                    to accompany you through every intricate step of the process. With a steadfast commitment to excellence
-                    and a wealth of experience in the field, we pride ourselves on delivering unparalleled guidance and
-                    support, ensuring that your legal endeavors are executed seamlessly and with utmost confidence.</p>
-            </div>
-        </div>
+    <section class="w-full md:px-40 px-5 md:py-24 py-16 flex flex-col justify-center md:items-start gap-5">
+        <p class="md:text-2xl text-[#1D1F21] text-center md:text-left">Notary Public Oshawa is your premier destination for professional document notarization services in Oshawa and beyond. With a dedicated team of experienced professionals, we strive to deliver efficient and reliable solutions for all your notarization needs. Our commitment to excellence is evident in every aspect of our work, from the meticulous attention to detail to the prompt and courteous service we provide to our valued clients. Whether you're in need of notarizing documents, preparing affidavits, or seeking assistance with Commissioner of Oath duties, you can trust Notary Public Oshawa to deliver results that exceed your expectations.</p>
 
-        <div class="md:w-1/2">
-            <img src="{{ asset('images/about.jfif') }}" alt="" class="w-full rounded-t-lg">
-            <div class="mt-6">
-                <h1 class="text-[#121212] text-center md:text-left font-[Inter] text-4xl font-medium">Our Mission</h1>
-                <p class="mt-2 text-center md:text-left">
-                    At [Company Name], our mission is clear: to provide exceptional notary and legal services that go above
-                    and beyond our clients' expectations. We adhere to the highest standards of professionalism, integrity,
-                    and reliability in every aspect of our work. From personalized attention to expert guidance, we strive
-                    to ensure that every client feels supported and empowered throughout their journey with us. With
-                    [Company Name], you can trust that your legal matters are in capable hands, and we are dedicated to
-                    achieving the best possible outcomes for you.
-                </p>
-            </div>
-        </div>
+        <p class="md:text-3xl text-[#1D1F21] text-center md:text-left font-semibold">Our Mission</p>
+
+        <p class="md:text-2xl text-[#1D1F21] text-center md:text-left">Our mission at Notary Public Oshawa is to provide top-notch notarization services with utmost efficiency and professionalism. We aim to simplify the notarization process for our clients, ensuring their documents are handled accurately and confidentially. With a focus on excellence and customer satisfaction, we strive to be your trusted partner for all your notarization needs.</p>
     </section>
 
 
@@ -50,48 +29,46 @@
         <img src="{{ asset('images/about-2.png') }}" alt="">
         <div class="absolute bottom-0 md:py-16 py-8 md:px-20 px-5 bg-[#004D80]">
             <p class="text-white text-center font-[Inter] md:text-3xl">
-                We are committed to building long-term relationships with our clients based on trust, reliability, and
-                mutual respect. Whether you need a simple document notarized or complex legal advice, you can rely on us to
-                provide prompt, professional, and personalized service every step of the way.
+                Count on us to be your trusted ally in navigating the intricacies of notarization and legal documentation. At Notary Public Oshawa, your satisfaction is our priority, and we strive to exceed your expectations with every interaction.
             </p>
         </div>
     </section>
 
     <section class="flex flex-col md:flex-row pt-24 md:px-20 px-5 justify-center md:gap-16 gap-10">
         <div class="md:w-1/2">
-            <img src="{{ asset("images/about.png") }}" alt="" class="h-full">
+            <img src="{{ asset("images/notary-values.png") }}" alt="" class="h-full">
         </div>
         <div class="md:w-1/2">
-            <h1 class="md:text-6xl text-center md:text-left text-4xl font-[Inter] text-[#121212] font-semibold">Our Values</h1>
-            <div class="mt-6">
-                <h3 class="text-2xl text-center md:text-left ">Professionalism</h3>
-                <p class="mt-2 text-[#1D1F21] text-center md:text-left ">We adhere unwaveringly to the highest standards of professionalism and ethics in everything we do, ensuring integrity, trust, and excellence.</p>
+            <h1 class="md:text-5xl text-center md:text-left text-3xl font-[Inter] text-[#121212] font-semibold">Our Values</h1>
+            <div class="md:mt-6 mt-4">
+                <h3 class="md:text-2xl text-xl font-semibold text-center md:text-left ">Professionalism</h3>
+                <p class="mt-2 text-[#53595F] text-center md:text-left ">We adhere unwaveringly to the highest standards of professionalism and ethics in everything we do, ensuring integrity, trust, and excellence.</p>
             </div>
 
             <div class="mt-6">
-                <h3 class="text-2xl text-center md:text-left ">Integrity</h3>
-                <p class="mt-2 text-[#1D1F21] text-center md:text-left ">
+                <h3 class="md:text-2xl text-xl font-semibold text-center md:text-left ">Integrity</h3>
+                <p class="mt-2 text-[#53595F] text-center md:text-left ">
                     We conduct our business with honesty, transparency, and integrity, earning the trust and respect of our clients.
                 </p>
             </div>
 
             <div class="mt-6">
-                <h3 class="text-2xl text-center md:text-left ">Client-Centric Approach</h3>
-                <p class="mt-2 text-[#1D1F21] text-center md:text-left ">
+                <h3 class="md:text-2xl text-xl font-semibold text-center md:text-left ">Client-Centric Approach</h3>
+                <p class="mt-2 text-[#53595F] text-center md:text-left ">
                     We prioritize the needs and satisfaction of our clients, striving to provide personalized solutions and exceptional service.
                 </p>
             </div>
 
             <div class="mt-6">
-                <h3 class="text-2xl text-center md:text-left ">Innovation</h3>
-                <p class="mt-2 text-[#1D1F21] text-center md:text-left ">
+                <h3 class="md:text-2xl text-xl font-semibold text-center md:text-left ">Innovation</h3>
+                <p class="mt-2 text-[#53595F] text-center md:text-left ">
                     We embrace innovation and technology to enhance the efficiency and effectiveness of our services, staying ahead of industry trends and advancements.
                 </p>
             </div>
 
             <div class="mt-6">
-                <h3 class="text-2xl text-center md:text-left ">Community Engagement</h3>
-                <p class="mt-2 text-[#1D1F21] text-center md:text-left ">
+                <h3 class="md:text-2xl text-xl font-semibold text-center md:text-left ">Community Engagement</h3>
+                <p class="mt-2 text-[#53595F] text-center md:text-left ">
                     We are committed to giving back to the communities we serve, supporting local initiatives and organizations.
                 </p>
             </div>
